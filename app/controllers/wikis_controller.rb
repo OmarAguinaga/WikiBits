29ca6c3997969f8b8b48df57d7449d214cbf1db0
Omar <omar.aguinaga94@gmail.com>
2017-07-11 14:10:28 +0000

    Premium and Admin users are able to create private wikis

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -66,7 +66,7 @@ class WikisController < ApplicationController
 
   private
   def wiki_params
-    params.require(:wiki).permit(:title, :body)
+    params.require(:wiki).permit(:title, :body, :private)
   end
 
   def authorize_user
